6a5e4ec553fcfd4f2dd193e9276d750a6264149d
wu979 <18683789594@163.com>
2018-11-30 13:26:32 +0800

    第一次

---
+++ src/main/java/com/hd/createcode/CreateCode.java
@@ -0,0 +1,54 @@
+package com.hd.createcode;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.JavaElement;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+/**
+ * @des:
+ * @author: wsw
+ * @email: 18683789594@163.com
+ * @date: 2018/9/29 9:45.
+ */
+public class CreateCode   extends DefaultCommentGenerator {
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        // 添加字段注释
+        if (introspectedColumn.getRemarks() != null) {
+            field.addJavaDocLine("/**" + introspectedColumn.getRemarks() + "*/");
+        }
+        System.out.println(introspectedColumn.getRemarks());
+    }
+
+    @Override
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
+        method.addJavaDocLine("");
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
+        method.addJavaDocLine("");
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+        method.addJavaDocLine("");
+    }
+
+    @Override
+    protected void addJavadocTag(JavaElement javaElement, boolean markAsDoNotDelete) {
+        // TODO Auto-generated method stub
+        javaElement.addJavaDocLine("");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
